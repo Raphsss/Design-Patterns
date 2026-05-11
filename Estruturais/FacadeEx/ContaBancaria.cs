1a936ef8bb77f49ca7704a3d30ee4fb83ca575e3
Raphael Lucas <rafaoliveira2009@Outlook.com>
2026-05-11 03:33:42 -0300

    .

---
+++ Estruturais/FacadeEx/ContaBancaria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeEx
+{
+    public class ContaBancaria
+    {
+        public string NumeroConta { get; }
+        public string Titular { get; }
+        public decimal Saldo { get; private set; }
+
+        public ContaBancaria(string NumeroConta, string Titular)
+        {
+            this.NumeroConta = NumeroConta;
+            this.Titular = Titular;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            Saldo -= valor;
+        }
+    }
+}
---
+++ Estruturais/FacadeEx/ValidarConta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeEx
+{
+    internal class ValidarConta
+    {
+    }
+}
