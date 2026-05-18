0cc50b3d35d3354c3ab8d75cde3e9b0455b5a927
Raphael Lucas <rafaoliveira2009@Outlook.com>
2026-05-18 09:19:26 -0300

    Singleton

---
+++ Criacionais/SingletonEx/Logger.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletonEx
+{
+    public class Logger
+    {
+        private static Logger _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null) { _instance = new Logger(); }
+  
+                return _instance;
+            }
+        }
+
+        private Logger() { } // Evita que se possa istanciar a classe diretamente
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
---
+++ Criacionais/SingletonEx/Program.cs
@@ -0,0 +1,10 @@
+﻿using SingletonEx;
+
+public class Program()
+{
+    public static void Main(string[] args)
+    {
+        Logger logger = Logger.Instance;
+        logger.Log("Singleton implementado!");
+    }
+}
